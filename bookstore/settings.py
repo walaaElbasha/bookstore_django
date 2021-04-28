f9a96535fbebbd0e3649257b08b8e6a91cd470b2
walaaElbasha <52048396+walaaElbasha@users.noreply.github.com>
2021-04-28 17:03:25 +0200

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #"accounts.middleware.SimpleMiddleware"
+    "accounts.middleware.SimpleMiddleware"
 ]
 
 REST_FRAMEWORK = {
